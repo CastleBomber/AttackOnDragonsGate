ee9d36b91200c1299fe39d5c76dec10e46c4c410
cBombs <richie.ucsb@gmail.com>
2019-08-01 01:09:11 -0700

    added some rough movement patterns L, R

--- raiseYourHandsUp.py
+++ raiseYourHandsUp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-sudo ./raiseYourHandsUp.py --led-rows=32 --led-cols=32  --led-brightness=78 --led-pwm-lsb-nanoseconds=300 --led-slowdown-gpio=2
+sudo ./raiseYourHandsUp.py --led-rows=32 --led-cols=32  --led-brightness=40 --led-pwm-lsb-nanoseconds=300 --led-slowdown-gpio=2
 '''
 from samplebase import SampleBase
 import pyaudio
@@ -67,14 +67,21 @@ def calculate_levels(data, chunk, sample_rate):
 height = {9:0,8:0,7:0,6:0,5:0,4:0,3:0,2:0,1:0,0:0}
 
 '''
+    CastleBomber sigil/ show
 '''
 class LightShow(SampleBase):
     def __init__(self, *args, **kwargs):
         super(LightShow, self).__init__(*args, **kwargs)
     
     def run(self):
+        self.goLeft()
+        self.goRight()
         self.rndmFlipThrough()
 
+    '''
+        Inside the folder 'pixelSheets' are "sketches"
+        flips through images (have yet to work the ordering)
+    '''
     def rndmFlipThrough(self):
         canvas = self.matrix.CreateFrameCanvas()
         while True:
@@ -94,10 +101,59 @@ class LightShow(SampleBase):
                 self.usleep(999999)
                 fo.close()
 
+    '''
+        Sigil goes left,
+        set up count for 32 steps
+        would slowly slide up if not stopped
+    '''
+    def goLeft(self):
+        canvas = self.matrix.CreateFrameCanvas()
+        fo = open('/home/pi/Desktop/pixelSheets/pixels_2bw')
+        pixelStr = fo.read()
+        pixelStr = re.sub(r"[\n\t\s]*", "", pixelStr)
+        count = 0
 
+        while (count < 32):
+            for y in range(0, 32):
+                    for x in range(0, 32):
+                        pixelPos = pixelStr[x+(y*32)]
+                        canvas.SetPixel( x, y,
+                                        d[pixelPos][0],
+                                        d[pixelPos][1],
+                                        d[pixelPos][2])
+            canvas = self.matrix.SwapOnVSync(canvas)
+            self.usleep(99999)
+            headlessStr = pixelStr[1:]
+            pixelStr = headlessStr + pixelStr[0]
+            count += 1
+            
+        fo.close()
 
+    def goRight(self):
+        canvas = self.matrix.CreateFrameCanvas()
+        fo = open('/home/pi/Desktop/pixelSheets/pixels_3bw')
+        pixelStr = fo.read()
+        pixelStr = re.sub(r"[\n\t\s]*", "", pixelStr)
+        count = 0
 
-# Main function
+        while (count < 32):
+            for y in range(0, 32):
+                    for x in range(0, 32):
+                        pixelPos = pixelStr[x+(y*32)]
+                        canvas.SetPixel( x, y,
+                                        d[pixelPos][0],
+                                        d[pixelPos][1],
+                                        d[pixelPos][2])
+            canvas = self.matrix.SwapOnVSync(canvas)
+            self.usleep(99999)
+            taillessStr = pixelStr[:-1]
+            pixelStr =  pixelStr[-1:] + taillessStr
+            count += 1
+            
+        fo.close()
+
+        
+
 if __name__ == "__main__":
     lightShow_driver = LightShow()
     if (not lightShow_driver.process()):
